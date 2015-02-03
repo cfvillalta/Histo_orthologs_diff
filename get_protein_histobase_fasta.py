6cf7d86f1c2b109487eb93fd348d8e86d999bfe5
Chris Villalta <chris.villalta@ucsf.edu>
2015-02-03 12:39:19 -0800

    need to fix error name seq not defined

--- get_protein_histobase_fasta.py
+++ get_protein_histobase_fasta.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python 
+#The script can have a gene id input and will output the protein sequence extracted from histobase.
+
+import sys
+
+id_in = sys.argv[1]
+type_in =  sys.argv[2]
+
+from GenomeFactory import GenomeFactory
+
+f = GenomeFactory()
+
+gene = f.getGene(id_in)
+
+prot_seq = gene.ProteinSequence()
+
+print seq.FormatFasta(name=gene.Name(), annotation = type_in)
