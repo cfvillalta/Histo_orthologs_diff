6a4d7465742074e99e40811c289db36ab2c49756
Mark Voorhies <mark.voorhies@ucsf.edu>
2015-02-03 16:04:29 -0800

    Code-reviewed version

--- get_protein_histobase_fasta.py
+++ get_protein_histobase_fasta.py
@@ -1,17 +1,43 @@
-#!/usr/bin/python 
-#The script can have a gene id input and will output the protein sequence extracted from histobase.
+#!/usr/bin/python
+#  chmod "a+x" get_protein_histobase_fasta.py to take advantage of the shebang.
+#
+#  Top level documentation as string.  Doesn't matter for script,
+#  but for an importable module, this is used as the top-level
+#  docstring.
+"""The script can have a gene id input and will output the protein
+sequence extracted from histobase.
+"""
 
+# Imports at top of file, external dependencies followed by local dependencies.
+#  [This is the convention in the Python world.  I often reverse this order
+#   in HistoBase, but I am wrong =) ]
 import sys
-
-id_in = sys.argv[1]
-type_in =  sys.argv[2]
 
 from GenomeFactory import GenomeFactory
 
-f = GenomeFactory()
+# Distinguish "main" function from local functions and variables.
+# (Aids readability and makes it easier to turn scripts into library modules)
 
-gene = f.getGene(id_in)
+if(__name__ == "__main__"):
+    # Script with wrong arguments should print documentation.
+    # Where possible, script with no arguments should also print documentation.
+    if(len(sys.argv) != 3):
+        sys.stderr.write("""Usage: %s id_in type_in > id_in.fasta
+   where:
+      id_in is the systematic gene name and
+      type_in is the annotation to be passed to the output FASTA file
+""" % sys.argv[0])
+        # exit with error code
+        sys.exit(0)
+        
+    id_in = sys.argv[1]
+    # I would make type_in optional and provide a sane default (e.g.,
+    # no annotation), updating the documentation above.
+    type_in =  sys.argv[2]
 
-prot_seq = gene.ProteinSequence()
+    f = GenomeFactory()
+    gene = f.getGene(id_in)
+    prot_seq = gene.ProteinSequence()
 
-print seq.FormatFasta(name=gene.Name(), annotation = type_in)
+    # BUGFIX: fixed variable name
+    print prot_seq.FormatFasta(name=gene.Name(), annotation = type_in)
