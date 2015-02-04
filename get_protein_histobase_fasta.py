537a981de1684e6058d90684adf6318a28933a38
Chris Villalta <chris.villalta@ucsf.edu>
2015-02-04 15:48:39 -0800

    fixed script with more proper coding convetions.

--- get_protein_histobase_fasta.py
+++ get_protein_histobase_fasta.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python 
-#The script can have a gene id input and will output the protein sequence extracted from histobase.
+"""
+The script can have a gene id input and will output the protein sequence extracted from histobase.
+"""
 
 import sys
-
-id_in = sys.argv[1]
-type_in =  sys.argv[2]
-
 from GenomeFactory import GenomeFactory
 
-f = GenomeFactory()
-
-gene = f.getGene(id_in)
-
-prot_seq = gene.ProteinSequence()
-
-print prot_seq.FormatFasta(name=gene.Name(), annotation = type_in)
+if(__name__ == "__main__"):
+    if(len(sys.argv) != 3):
+        sys.stderr.write("""Usage: %s id_in type_in > id_in.fasta
+        where:
+        id_in is the systemic gene name
+        type_in is the annotation to be passed to the output FASTA file"""%(sys.argv[0]))
+        sys.exit(0)
+    else:    
+        id_in = sys.argv[1]
+        type_in =  sys.argv[2]
+        f = GenomeFactory()
+        gene = f.getGene(id_in)
+        prot_seq = gene.ProteinSequence()
+        #BUGFIX: fixed variable name
+        print prot_seq.FormatFasta(name=gene.Name(), annotation = type_in)
