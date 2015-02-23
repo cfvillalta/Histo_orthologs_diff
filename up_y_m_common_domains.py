0278c04b9597a02a69700e4d2743b31a9288d76a
Chris Villalta <cfvillalta@gmail.com>
2015-02-23 15:30:49 -0800

    Outputs aa freq and numbers for pfam groups

--- up_y_m_common_domains.py
+++ up_y_m_common_domains.py
@@ -11,6 +11,22 @@ from CdtFile import CdtFile, CdtRow
 from GenomeFactory import GenomeFactory
 from optparse import OptionParser
 
+#Copy code in from CompositionHeatmap.py to get amino acid freqs.
+
+# N.B.: Don't want silent out-of-range bugs, so not doing ascii math
+amino_acids = "ACDEFGHIKLMNPQRSTVWY"
+aa2offset = dict((aa,i) for (i,aa) in enumerate(amino_acids))
+for (key,val) in aa2offset.items():
+    aa2offset[key.lower()] = val
+    
+def composition_vector(s):
+    v = [0.]*20
+    for i in s:
+        try:
+            v[aa2offset[i]] += 1.
+        except KeyError:
+            pass
+    return v
 
 if(__name__=="__main__"):
     # Input and find common pfam domains between Yeast and Mycelia pfam
@@ -26,10 +42,10 @@ if(__name__=="__main__"):
 
     # Search through yeast and mycelia sig up reg .cdt files and get
     # gene names for genes that have the common domains found in shared_domains
-
-    cdt_in = CdtFile.fromCdt(open("/home/cfvillalta/ThermalAdaptation/s13cMsort.cdt"))
-    up_m_genes_in = open("/home/cfvillalta/ThermalAdaptation/M_1.5_transcripts.HcG217B.txt")
-    up_y_genes_in = open("/home/cfvillalta/ThermalAdaptation/Y_1.5_transcripts.HcG217B.txt")
+    cdt_file = "/home/cfvillalta/ThermalAdaptation/s13cYsort.cdt"
+    cdt_in = CdtFile.fromCdt(open(cdt_file))
+    up_m_genes_in = open("/home/cfvillalta/ThermalAdaptation/M_1.5_predictions.HcG217B.txt")
+    up_y_genes_in = open("/home/cfvillalta/ThermalAdaptation/Y_1.5_predictions.HcG217B.txt")
 
     cdt_gene_pfam = {} 
     pfam_indices = [n for (n,i) in enumerate(cdt_in.extranames)
@@ -39,8 +55,10 @@ if(__name__=="__main__"):
         for i in pfam_indices:
             pfam_name_split.update(gene.extra[i].split("|"))
         for name in gene.Name().split("|"):
-            cdt_gene_pfam[gene.extra[2]]=pfam_name_split
-
+            #some genes have two names separated by |
+            cdt_gene_pfam[name]=pfam_name_split
+            #gene.extra[2] prints out the transcirpt name.
+            #print cdt_gene_pfam
     up_m_genes = dict((gene.strip(), cdt_gene_pfam[gene.strip()])
                       for gene in up_m_genes_in)
 
@@ -81,25 +99,27 @@ if(__name__=="__main__"):
 
             if y>=1 and m >=1:
                 shared_domain_genes[domain]=shared_domains[domain]
-#print shared_domain_genes
-
-#Copy code in from CompositionHeatmap.py to get amino acid freqs.
-
-# N.B.: Don't want silent out-of-range bugs, so not doing ascii math
-amino_acids = "ACDEFGHIKLMNPQRSTVWY"
-aa2offset = dict((aa,i) for (i,aa) in enumerate(
-    amino_acids))
-for (key,val) in aa2offset.items():
-    aa2offset[key.lower()] = val
-    
-    def composition_vector(s):
-        v = [0.]*20
-        for i in s:
-            try:
-                v[aa2offset[i]] += 1.
-            except KeyError:
-                pass
-            return v
-        
-
-    
+    #print shared_domain_genes
+    f=GenomeFactory()
+    genes =[]
+    for domain in shared_domain_genes:
+        for g in shared_domain_genes[domain]: 
+            gene = f.getGene(g[0])
+            seq = gene.ProteinSequence()
+            #print seq
+            genes.append([gene,seq])
+    #        print seq
+     #       print composition_vector(seq)
+    #print genes
+    cdt = CdtFile(probes = [CdtRow(gid = gene[0].Name(),uniqid = gene[0].Name(),name = gene[0].Name(),ratios = [len(gene[1])]+composition_vector(gene[1]))for gene in genes if(len(gene[1]) > 0)],fieldnames = ["length"]+list(amino_acids),eweights = [1.]*21)
+     
+     #Normalize counts to length
+    cdt = CdtFile.fromPrototype(cdt,probes = [CdtRow.fromPrototype(i,ratios = i.ratios+[j/float(i.ratios[0]) for j in i[1:]])for i in cdt],eweights = cdt.eweights + [1.]*20,fieldnames = cdt.fieldnames + [j.lower() for j in cdt.fieldnames[1:]])
+     
+    # Write unsorted data
+    filename = cdt_file.split(".cdt")
+    cdt.writeCdt(open("%s_aa_ratio.cdt" %(filename[0]),"w"))
+     
+    #Cluster on normalized counts
+    tree = cdt.cluster(dist = "e", method = "m", cols = range(21,41))
+    cdt.writeCdtGtr("%s.composition.norm_em" %(filename[0]), tree)
