f0f2fd7903d90f1aa2c2e150d2810130b9435e80
Chris Villalta <cfvillalta@gmail.com>
2015-02-26 15:48:44 -0800

    Can import MSA stockholm file

--- stockholm_in_ungapped_aa_ratios_out.py
+++ stockholm_in_ungapped_aa_ratios_out.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python 
+
+from ClustalTools import MultipleAlignment
+if(__name__=="__main__"):
+    hmmalign = open("/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/Lucien_Zn_clu/Zn_clus.hmmalign")
+    alignment = MultipleAlignment.fromStockholm(hmmalign)
+
+    print alignment
