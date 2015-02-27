b71cc68c55afe16eb2c12934e74f24285b934a9c
Chris Villalta <cfvillalta@gmail.com>
2015-02-27 10:05:04 -0800

    can output nongapped seqs and seq name.

--- stockholm_in_ungapped_aa_ratios_out.py
+++ stockholm_in_ungapped_aa_ratios_out.py
@@ -2,13 +2,18 @@
 
 from ClustalTools import MultipleAlignment
 if(__name__=="__main__"):
-    hmmalign = open("/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/Lucien_Zn_clu/Zn_clus.hmmalign")
+    hmmalign = open("/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/Lucien_Zn_clu/Zn_clus_test.stockholm")
     alignment = MultipleAlignment.fromStockholm(hmmalign)
-
+#    print alignment.seqnames
     ref= alignment.colAnnotations
-    print ref.keys()
+#    print ref.keys()
     x_indices = [n for (n,i) in enumerate(ref['RF']) if (i=="x")]
-    print x_indices
+    #print x_indices
 
-    for x in x_indices:
+    for (seq,name) in zip(alignment, alignment.seqnames):
+        print "%s\n%s" %(name, "".join(seq))
+        #print x.seqnames
+        
+        
+        
         
