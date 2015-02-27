2e3f3bf6f850e3efaae4122f2a766c03c69fc123
Chris Villalta <cfvillalta@gmail.com>
2015-02-27 13:03:57 -0800

    print out pdf with histogram

--- stockholm_in_ungapped_aa_ratios_out.py
+++ stockholm_in_ungapped_aa_ratios_out.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python 
 
+import numpy as np
+import matplotlib as mpl
+mpl.use('agg')
+import matplotlib.pyplot as plt
+
 # N.B.: Don't want silent out-of-range bugs, so not doing ascii math
 amino_acids = "ACDEFGHIKLMNPQRSTVWY"
 aa2offset = dict((aa,i) for (i,aa) in enumerate(amino_acids))
@@ -19,22 +24,35 @@ for (key,val) in aa2offset.items():
 
 from ClustalTools import MultipleAlignment
 if(__name__=="__main__"):
-    hmmalign = open("/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/Lucien_Zn_clu/Zn_clus_test.stockholm")
+    hmmalign = open("/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/Lucien_Zn_clu/Zn_clus.hmmalign")
     alignment = MultipleAlignment.fromStockholm(hmmalign)
 #    print alignment.seqnames
     ref= alignment.colAnnotations
 #    print ref.keys()
     x_indices = [n for (n,i) in enumerate(ref['RF']) if (i=="x")]
     #print x_indices
-
+    aa_data = []
     for (seq,name) in zip(alignment, alignment.seqnames):
-        print "%s\n%s" %(name, "".join(seq))
+        #print "%s\n%s" %(name, "".join(seq))
         aa_counts = composition_vector(seq)
         aa_ratios = [float(aa/sum(aa_counts)) for aa in aa_counts]
-        print aa_ratios
-        print aa_counts
-        print sum(aa_counts)
-        
-        
-        
-        
+        #print aa_ratios
+        #print aa_counts
+        #print sum(aa_counts)
+        aa_data.append([name,aa_counts,aa_ratios])
+
+#print aa_data
+A = []
+for aa in aa_data:
+ #   print aa
+    A.append(aa[2][0])
+
+
+x = np.random.normal(0,1,1000)
+#print x
+fig = plt.figure()
+ax = fig.add_subplot(111)
+numBins = 100
+histo=ax.hist(A,numBins,color='green',alpha=0.8)
+fig.savefig('fig1.pdf')
+
