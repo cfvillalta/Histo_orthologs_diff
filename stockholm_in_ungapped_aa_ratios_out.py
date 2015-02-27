efbfc9de332fe2c91ecdcd9781dca8ef377510ea
Chris Villalta <cfvillalta@gmail.com>
2015-02-26 16:29:51 -0800

    Working on using indices to collect seqs, stopped to head home

--- stockholm_in_ungapped_aa_ratios_out.py
+++ stockholm_in_ungapped_aa_ratios_out.py
@@ -5,4 +5,10 @@ if(__name__=="__main__"):
     hmmalign = open("/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/Lucien_Zn_clu/Zn_clus.hmmalign")
     alignment = MultipleAlignment.fromStockholm(hmmalign)
 
-    print alignment
+    ref= alignment.colAnnotations
+    print ref.keys()
+    x_indices = [n for (n,i) in enumerate(ref['RF']) if (i=="x")]
+    print x_indices
+
+    for x in x_indices:
+        
