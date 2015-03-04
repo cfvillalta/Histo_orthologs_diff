9a986d10dfb1434f6e54898c7b1e81e97ee46cdc
Chris Villalta <cfvillalta@gmail.com>
2015-03-03 21:15:16 -0800

    ready to run script with all domains,willoutput 92 subplots with domain name and aa labeled

--- domains_in_boxplots_out.py
+++ domains_in_boxplots_out.py
@@ -4,32 +4,57 @@ Will input list of domains, run lucien with histo transcriptome data for each do
 '''
 
 from hmmalign2norm import aa_mean_normilization
-#import numpy as np
-#import matplotlib as mpl
-#mpl.use('agg')
-#import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 from subprocess import Popen, PIPE
 import os
 
 
 if(__name__=="__main__"):
-    domain_in = "/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/pfam_test.txt"
+    domain_in = "/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/yeast_mycelia_pfam_up.txt"
     domain_list = open(domain_in)
 
     domains = [i.strip() for i in domain_list.readlines()]
+    domains_aa_norm = []
+    domains_len=len(domains)
+    num=0
     for domain in domains:
-        print 'running Lucien.py on %s' %(domain)
-#        Lucien = Popen(['Lucien.py', '-D', domain, '-G', '-g', '/home/cfvillalta/AssembledTranscriptomes/ForLucien/'],stdout=PIPE)
+        num=num+1
+        print 'running Lucien.py on %s, %s out of %s' %(domain,num,domains_len)
+        Lucien = Popen(['Lucien.py', '-D', domain, '-G', '-g', '/home/cfvillalta/AssembledTranscriptomes/ForLucien/'],stdout=PIPE)
         
         Lucien.communicate()
-        print 'Luncien run done.'
+        print 'Lucien run done.'
 
         hmmalign =open("%s.hmmalign" %(domain))
         aa_mean_norm = aa_mean_normilization(hmmalign)
-    
+        domains_aa_norm.append(aa_mean_norm)
     #dir =  os.path.dirname(os.path.abspath(domain_in))
-    
-    
+
+#    print domains_len
+    bp_range = range(10)
+    bp_cord = []
+    for x in bp_range:
+        for y in bp_range:
+            bp_cord.append([x,y])
+    #print bp_cord
+
+    f, ax = plt.subplots(10,10,sharex=True,sharey=True)
+    f.set_size_inches(40,40)
+    plt.tight_layout()
+    amino_acids = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
+    for x in range(domains_len):
+        ax[bp_cord[x][0],bp_cord[x][1]].boxplot(domains_aa_norm[x][2])
+        #print domains[x]
+        ax[bp_cord[x][0],bp_cord[x][1]].set_title('%s' %(domains[x]))
+        ax[bp_cord[x][0],bp_cord[x][1]].set_xticklabels(amino_acids)
+    #ax.plot(x, y)
+    #ax.set_title('Simple plot')
+    #fig=plt.subplots(1,3)
+    #plt.show()
+    #print 'done'
+    #print plt.get_backend()
+    f.savefig('mean_normalized.pdf')
 #print domains
 
 '''
