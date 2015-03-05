bd6243c8f04db544a7eb26b713eb32942125cd90
Chris Villalta <cfvillalta@gmail.com>
2015-03-05 13:26:33 -0800

    added an -L option to force run lucien, and now the script checks if an hmmalign file is present to determine if it has to run lucien.

--- domains_in_boxplots_out.py
+++ domains_in_boxplots_out.py
@@ -2,17 +2,17 @@
 '''
 Will input list of domains, run lucien with histo transcriptome data for each domain, from each hmm domain alignment will get amino acid ratios and normalize by mean. Plot unnormalized and normalized data boxplots.
 '''
-
+import sys
+import re
 from hmmalign2norm import aa_mean_normilization
 import numpy as np
 import matplotlib.pyplot as plt
 from subprocess import Popen, PIPE
 import os
 
-
 if(__name__=="__main__"):
-    domain_in = "/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/yeast_mycelia_pfam_up.txt"
-#    domain_in = "/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/pfam_test.txt"
+#    domain_in = "/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/yeast_mycelia_pfam_up.txt"
+    domain_in = "/home/cfvillalta/ThermalAdaptation/thermal_domain_ratio_20150226/pfam_test.txt"
 
     domain_list = open(domain_in)
 
@@ -22,23 +22,30 @@ if(__name__=="__main__"):
     num=0
     for domain in domains:
         num=num+1
-        print 'running Lucien.py on %s, %s out of %s' %(domain,num,domains_len)
-        
-        Lucien = Popen(['Lucien.py', '-D', domain, '-G', '-g', '/home/cfvillalta/AssembledTranscriptomes/ForLucien/'],stdout=PIPE)
-        
-        Lucien.communicate()
-        print 'Lucien run done.'
-        if os.stat("%s.hmmalign" %(domain)).st_size == 0:
-            print "Sequence file is empty or misformatted"
-            domains_aa_norm.append('None')
+        #if this option present force run Lucien.py
+        force_lucien = re.compile('-L')
+        run_lucien = force_lucien.search(str(sys.argv))
+        if run_lucien: 
+            print 'running Lucien.py on %s, %s out of %s' %(domain,num,domains_len)
+            Lucien = Popen(['Lucien.py','-a', '-D', domain, '-G', '-g', '/home/cfvillalta/AssembledTranscriptomes/ForLucien/'],stdout=PIPE)
+            Lucien.communicate()
+            print 'Lucien run done.'
+
+        if os.path.isfile("%s.hmmalign" %(domain)):
+            print 'hmmalign file present. Lucien will not run on %s, %s out of %s' %(domain,num,domains_len)
+            if os.stat("%s.hmmalign" %(domain)).st_size == 0:
+                print "Hmmalign file for %s is empty or misformatted" %(domain)
+                domains_aa_norm.append('None')
+            else:
+                hmmalign =open("%s.hmmalign" %(domain))
+                aa_mean_norm = aa_mean_normilization(hmmalign)
+                domains_aa_norm.append(aa_mean_norm)            
         else:
-            hmmalign =open("%s.hmmalign" %(domain))
-            aa_mean_norm = aa_mean_normilization(hmmalign)
-            domains_aa_norm.append(aa_mean_norm)
+            print 'running Lucien.py on %s, %s out of %s' %(domain,num,domains_len)
+            Lucien = Popen(['Lucien.py','-a', '-D', domain, '-G', '-g', '/home/cfvillalta/AssembledTranscriptomes/ForLucien/'],stdout=PIPE)
+            Lucien.communicate()
+            print 'Lucien run done.'
 
-    #dir =  os.path.dirname(os.path.abspath(domain_in))
-
-#    print domains_len
     bp_range = range(10)
     bp_cord = []
     for x in bp_range:
