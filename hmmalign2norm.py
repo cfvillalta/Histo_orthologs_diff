a3026e5884eeebc4aacb716d6ea05d92ede8d90f
Chris Villalta <cfvillalta@gmail.com>
2015-03-13 09:38:40 -0700

    added a module aa_ratio_data to output important infomration for eventaul export as tab delimited

--- hmmalign2norm.py
+++ hmmalign2norm.py
@@ -85,9 +85,6 @@ def aa_mean_gene(goi):
                 aa_counts.append(aa_c)
                 aa_ratios.append(aa_r)
 
-        aa_cm = np.array(aa_counts)
-        aa_rm = np.array(aa_ratios)
-        aa_mean = []
         aa_range=range(0,len(amino_acids))
         bp_aa_ratios = []
     
@@ -98,6 +95,53 @@ def aa_mean_gene(goi):
     
     return(goi_out)
 
+
+'''
+input domain and grab hmm alingment from that domain (should be in same directory) and output normalized and non normalized amino acid ratios for all proteins in the stockholm file that map to a refseq position.
+'''
+
+def aa_ratio_data(domain):
+    hmmalign=open("%s.hmmalign" %(domain))
+    
+    alignment = MultipleAlignment.fromStockholm(hmmalign)
+    ref= alignment.colAnnotations
+    print ref.keys()
+    x_indices = [n for (n,i) in enumerate(ref['RF']) if (i=="x")]
+    #print x_indices
+                  
+    aa_counts =[]
+    aa_ratios = []
+    
+    domain_out = []
+    for (seq,name) in zip(alignment, alignment.seqnames):
+        #print name
+        #print len(genes)
+            
+        #print name            
+        #print "%s\n%s" %(name, "".join(seq))
+        aa_c = composition_vector(seq)
+        aa_r = [float(aa/sum(aa_c)) for aa in aa_c]
+            
+        aa_ratios.append(aa_r)
+        domain_out.append([name,domain,aa_c,aa_r])
+            
+    aa_mean = []
+    aa_rm = np.array(aa_ratios)
+    [aa_mean.append(np.mean(aa_rm[:,x])) for x in range(len(amino_acids))]
+
+    
+    print len(amino_acids)
+    print aa_mean
+    for x in range(len(domain_out)):
+        aa_norm_ratios = []
+        for aa in range(len(amino_acids)):
+            aa_norm_ratios.append(domain_out[x][3][aa]-aa_mean[aa])
+        domain_out[x].append(aa_norm_ratios)
+        
+    return(domain_out)               
+        
+        
 #hmmalign =open("SMC_N.hmmalign")
+print aa_ratio_data("SMC_N")
 #print aa_mean_gene('HISTO_DM.Contig933.Fgenesh_Aspergillus.157.final_new', hmmalign)
 #print aa_mean_gene('HISTO_DA.Contig93-snap.315.final_new', hmmalign)
